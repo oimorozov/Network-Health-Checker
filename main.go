fc9ccaa777a31181e190341192e11b08ce674db2
oimorozov <oimorozov@edu.hse.ru>
2026-05-23 13:48:46 +0300

    Write Output to File

--- main.go
+++ main.go
@@ -48,20 +48,31 @@ func main() {
 		fmt.Printf("Failed to read file: %v\n", err)
 		os.Exit(1)
 	}
+
 	wg := sync.WaitGroup{}
 	semaphore := make(chan struct{}, 10)
 	servers := parseFile(file)
+
+	output_file, err := os.Create("output.txt")
+	if err != nil {
+		fmt.Printf("Failed to create output file: %v\n", err)
+		os.Exit(1)
+	}
+	defer output_file.Close()
+	output_file.WriteString("Health check results\n--------------------\n")
+
 	for _, serv := range servers {
 		wg.Add(1)
 		go func(s Server) {
 			defer wg.Done()
 			defer func() { <-semaphore }()
+
 			semaphore <- struct{}{}
 			conn, err := net.DialTimeout("tcp", s.URL, 3*time.Second)
 			if err != nil {
-				fmt.Printf("Failed to connect to %s:%v\n", s.Name, err)
+				output_file.WriteString(fmt.Sprintf("Failed to connect to %s:%v\n", s.Name, err))
 			} else {
-				fmt.Printf("Successfully connected to %s\n", s.Name)
+				output_file.WriteString(fmt.Sprintf("Successfully connected to %s\n", s.Name))
 				conn.Close()
 			}
 		}(serv)
